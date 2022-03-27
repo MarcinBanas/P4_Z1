2a9a393f57cf67217ff806d909dd9e4283190b29
MarcinBanas <Marrcin132@gmail.com>
2022-03-27 10:16:06 +0200

    Dodaj pliki projektów.

---
+++ P4_Z1/BazaDanych.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Z1
+{
+    internal class BazaDanych
+    {
+        private SqlConnection _connection;
+
+        public BazaDanych(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void DodajWpis()
+        {
+            _connection.Open();
+            var insertSql = "insert into mg.Kategorie (IDKategorii,NazwaKategorii,Opis) VALUES (@ID,@Nazwakat,@Opis)";
+            var insertCommand = new SqlCommand(insertSql, _connection);
+            Console.WriteLine("ID:");
+            var id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nazwa Kategori:");
+            var nazwakat = Console.ReadLine();
+            Console.WriteLine("Opis:");
+            var opis = Console.ReadLine();
+            insertCommand.Parameters.Add(new SqlParameter("ID", id));
+            insertCommand.Parameters.Add(new SqlParameter("Nazwakat", nazwakat));
+            insertCommand.Parameters.Add(new SqlParameter("Opis", opis));
+            insertCommand.ExecuteNonQuery();
+            _connection.Close();
+        }
+    }
+}
---
+++ P4_Z1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using P4_Z1;
+
+BazaDanych Northwind = new BazaDanych(@"Data Source=DESKTOP-9K2SBT4;Initial Catalog=ZNorthwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+
+Northwind.DodajWpis();
